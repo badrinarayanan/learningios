880e361746d78d0270aa5562c0ddf5c112d912f6
badrinarayanan <badri1992@gmail.com>
2016-10-23 17:28:55 +0530

    Add files via upload

---
+++ testing app/testing app/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  testing app
+//
+//  Created by Badri Narayananan Ravi on 23/10/16.
+//  Copyright © 2016 Badri Narayananan Ravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var test: UILabel!
+    
+   
+    @IBAction func btnpress(_ sender: UIButton) {
+        
+        
+         let title = sender.title(for: .normal)!
+        
+        test.text =  " Yo clicked \(title)  "
+        
+        
+        
+        
+    }
+}
+
